42e74fe86c37e77af685200f6184883986cb94cc
Raktim <raktimunreal4@gmail.com>
2026-08-12 10:28:45 +0530

    Add CLI menu and input handling to sav.java

--- sav.java
+++ sav.java
@@ -1,3 +1,52 @@
+import java.util.Scanner;
+
 public class sav {
-    
+    public static void cls() throws Exception {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start();
+    }
+
+    public static void main(String[] args) throws Exception{
+        cls();
+
+        System.out.println("Select a shape from the list below :::");
+        System.out.println("    1. Cube");
+        System.out.println("    2. Cuboid");
+        System.out.println("    3. Cylinder");
+        System.out.println("    4. Sphere");
+        System.out.println("    5. Cone");
+
+        Scanner scObj = new Scanner(System.in);
+        int selector;
+
+        while (true) {
+            System.out.print("\nEnter your choice from the above list ::: ");
+            
+            try {
+                selector = scObj.nextInt();
+
+                if (selector >= 1 && selector <= 5){
+                    break;
+                } else {
+                    System.err.println("Out of range input. Try again\n");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid Input");
+                scObj.nextLine();
+            }
+        }
+
+        if (selector == 1) {
+
+        } else if (selector == 2) {
+
+        } else if (selector == 2) {
+
+        } else if (selector == 2) {
+
+        } else {
+
+        }
+
+        scObj.close();
+    }
 }
